1e05622c891bb25da53967332261cb9c2d6cc051
gawandeabhishek <abhishekgawande1667@gmail.com>
2024-11-11 01:11:38 +0530

    new commit

--- src/components/ui/slider.tsx
+++ src/components/ui/slider.tsx
@@ -17,10 +17,10 @@ const Slider = React.forwardRef<
     )}
     {...props}
   >
-    <SliderPrimitive.Track className="relative h-1 w-full grow overflow-hidden rounded-full bg-primary/10">
+    <SliderPrimitive.Track className="relative h-1 w-full grow overflow-hidden rounded-full bg-primary/10 cursor-pointer">
       <SliderPrimitive.Range className="absolute h-full bg-primary group-hover:bg-secondary-foreground rounded-full" />
     </SliderPrimitive.Track>
-    <SliderPrimitive.Thumb className="block h-3 w-3 rounded-full bg-transparent group-hover:bg-primary transition-color focus-visible:outline-none disabled:pointer-events-none disabled:opacity-50" />
+    <SliderPrimitive.Thumb className="block h-3 w-3 rounded-full bg-transparent group-hover:bg-primary transition-color focus-visible:outline-none disabled:pointer-events-none disabled:opacity-50 cursor-pointer" />
   </SliderPrimitive.Root>
 ))
 Slider.displayName = SliderPrimitive.Root.displayName
