ba4c4e79c774ea0e3e355fe8c103bd5cecd39f22
gawandeabhishek <abhishekgawande1667@gmail.com>
2024-11-17 01:03:08 +0530

    added liked songs displaying page

--- src/app/(pages)/liked-songs/page.tsx
+++ src/app/(pages)/liked-songs/page.tsx
@@ -1,19 +1,47 @@
 import { onAuthenticateUser } from "@/actions/user";
 import AppCard from "@/components/global/card";
+import { db } from "@/lib/prisma";
 import { SongProps } from "@/types/song";
-import { notFound, redirect } from "next/navigation";
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 import React from "react";
 
 const page = async () => {
   const auth = await onAuthenticateUser();
-  if (auth.status === 400 || auth.status === 500 || auth.status === 404 || auth.status === 403) return redirect('/auth/sign-in');
-  const songData = await fetch(`${process.env.BASE_URL}/api/top-songs`);
-  if (!songData) return notFound();
+  if (auth.status === 400 || auth.status === 500 || auth.status === 404 || auth.status === 403)
+    return redirect("/auth/sign-in");
 
-  if (!songData) return notFound();
-  const song_data = await songData.json();
+  const user = await currentUser();
+  if (!user) {
+    return { status: 403 };
+  }
 
-  const likedSongs = await song_data?.data?.data?.results;
+  // Fetch user data and related liked songs
+  const res = await db.user.findUnique({
+    where: {
+      clerkId: user.id,
+    },
+    include: {
+      likedSongs: true, // Only include liked songs
+    },
+  });
+
+  if (!res) {
+    return { status: 403 };
+  }
+
+  // Fetch the details for each liked song
+  const likedSongs = await Promise.all(
+    res.likedSongs.map(async (song) => {
+      const response = await fetch(`${process.env.BASE_URL}/api/song/${song.songId}`);
+      const songData = await response.json();
+
+      return songData?.data?.data[0];
+    })
+  );
+
+  console.log(likedSongs); // This should now log the array of song details
+
   return (
     <div className="flex flex-wrap justify-center items-center lg:items-start lg:justify-start gap-2">
       {likedSongs.map((item: SongProps, key: number) => (
