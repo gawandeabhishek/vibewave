95ca06b2ef715255811afe4c577b67b12101b08b
gawandeabhishek <abhishekgawande1667@gmail.com>
2025-01-07 21:34:03 +0530

    protected onClick cards route

--- src/app/(pages)/[dataId]/[id]/layout.tsx
+++ src/app/(pages)/[dataId]/[id]/layout.tsx
@@ -1,10 +1,20 @@
+import { onAuthenticateUser } from "@/actions/user";
+import { redirect } from "next/navigation";
 import React from "react";
 
 type Props = {
   children: React.ReactNode;
 };
 
-const Layout = ({ children }: Props) => {
+const Layout =  async ({ children }: Props) => {
+  const auth = await onAuthenticateUser();
+  if (
+    auth.status === 400 ||
+    auth.status === 500 ||
+    auth.status === 404 ||
+    auth.status === 403
+  )
+    return redirect("/auth/sign-in");
   return (
     <div>
       {children}
